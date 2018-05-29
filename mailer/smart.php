8afd1cd4d8f2da768ed2e11ae78da5b73fcbfd03
Jane Ivanova <ivanovaea13.04@gmail.com>
2018-05-29 15:12:54 +0300

    add first

---
+++ mailer/smart.php
@@ -0,0 +1,41 @@
+<?php 
+
+$phone = $_POST['user_phone'];
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'janeivmail@gmail.com';                 // Наш логин
+$mail->Password = 'ASDCGbiouywer12sdf';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('janeivmail@gmail.com', 'Jane Ivanova');   // От кого письмо 
+$mail->addAddress('ivanovaea13.04@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Новая заявка с сайта';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: неизвестно <br>
+	Телефон: ' . $phone . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+  echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+   //header('location: ../thanks.html');
+}
+
+?>
